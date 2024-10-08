5a1117ca743672bf1d5e220fcfc88dcbb33d62e6
Aashish Ranjan <58885697+sk-shahsi@users.noreply.github.com>
2024-10-08 09:13:41 +0530

    Add files via upload
    update in entity.

---
+++ medcin/medcin/src/main/java/com/medcin/medcin/controller/MedicineController.java
@@ -0,0 +1,29 @@
+package com.medcin.medcin.controller;
+
+import com.medcin.medcin.entity.Medicine;
+import com.medcin.medcin.service.MedicineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MedicineController {
+
+    @Autowired
+    MedicineService medicineService;
+    @PostMapping("/medicines/{name}")
+    private List<Medicine> getMedicines(@PathVariable("name")String name){
+
+        return medicineService.findMedicineByName(name);
+
+    }
+    @PutMapping("/medicines")
+    private Integer AddMedicine(@RequestBody Medicine medicin){
+
+        medicineService.saveMedicine(medicin);
+        return Math.toIntExact(medicin.getId());
+
+    }
+
+}

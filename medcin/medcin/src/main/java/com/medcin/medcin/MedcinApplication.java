e1eff06b80b05b98651639fbac4dde4cb409b0c9
Aashish Ranjan <58885697+sk-shahsi@users.noreply.github.com>
2024-10-08 09:13:41 +0530

    Add files via upload
    update in entity.

---
+++ medcin/medcin/src/main/java/com/medcin/medcin/MedcinApplication.java
@@ -0,0 +1,13 @@
+package com.medcin.medcin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcinApplication.class, args);
+	}
+
+}
---
+++ medcin/medcin/src/test/java/com/medcin/medcin/MedcinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcin.medcin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedcinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
